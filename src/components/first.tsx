ed047c38e1e30d544bffb88bac883ef020921c09
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-04-05 12:30:10 -0300

    mudanças pedidas na primeira dobra

--- src/components/first.tsx
+++ src/components/first.tsx
@@ -5,15 +5,15 @@ import maquinario from '../img/maquinarios.png'
 export default component$(() => {
     return (
         <div class=" flex-col w-full bg-blue-900 bg-cover bg-center flex justify-center  lg:justify-between lg:min-h-[90vh] lg:bg-bg-1">
-            <div class="flex flex-col-reverse gap-12 lg:gap-24 pt-10 lg:pt-5 lg:min-h-[90vh] px-[5%] lg:px-2 lg:flex-row mx-auto text-left lg:justify-between">
-                <div class="basis-1/3 flex justify-center items-center">
-                    <div class="bg-blue-900 flex justify-center items-center rounded-xl self-center">
+            <div class="flex flex-col-reverse gap-12 lg:gap-24 pt-10 w-full lg:pt-5 lg:min-h-[90vh] px-[5%] lg:px-[3%] lg:flex-row mx-auto text-left lg:justify-between">
+                <div class="basis-1/3 flex justify-center lg:justify-start items-center">
+                    <div class="bg-blue-900 flex justify-start items-start rounded-xl lg:max-w-[70%]">
                         <Form />
                     </div>
                 </div>
                 <div class="lg:basis-1/3 flex flex-col justify-start items-start lg:text-right">
-                    <h1 class="text-white lg:text-[28px] font-black mx-auto lg:leading-[50px]">
-                        <span class="text-3xl lg:text-5xl">Maquinário pesado, com parcelas levíssimas.</span><br/>
+                    <h1 class="text-white lg:text-[26px] font-black mx-auto lg:leading-[50px]">
+                        <span class="text-3xl lg:text-4xl">Maquinário pesado, com parcelas levíssimas.</span><br/>
                         Adquira o seu produto de linha amarela com o<span class="text-[#FFC700]"> Consórcio XCMG</span>
                     </h1>
                     <h2 class="font-medium text-2xl text-white">
