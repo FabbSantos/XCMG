8e8280e18ee4b91986e5d03ac44d9409defd1034
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-05-04 16:40:59 -0300

    start to evenly

--- src/components/second.tsx
+++ src/components/second.tsx
@@ -9,7 +9,7 @@ export default component$(() => {
         <div id="consorcio" class="flex flex-col items-center justify-center bg-bg-3 bg-cover">
             <h2 class="uppercase text-white text-[1.3rem] font-bold md:text-[1.8rem] pt-10">BENEFÍCIOS DO CONSÓRCIO XCMG</h2>
             <div class=" flex flex-row justify-evenly w-full py-20">
-                <div class="md:max-w-[320px] flex flex-col justify-start items-center gap-2  basis-[23%] bg-white text-[#003E88] rounded-xl pt-5">
+                <div class="md:max-w-[320px] flex flex-col justify-evenly items-center gap-2  basis-[23%] bg-white text-[#003E88] rounded-xl pt-5">
                     <img src={cofrinho} alt="Menor custo do mercado" loading="lazy" width="80px" height="80px"/>
                     <h2 class="text-xl min-h-[1.5em] text-left font-bold mx-2">MENOR CUSTO DO MERCADO</h2>
                     <ul class="max-w-[90%] text-center flex flex-col items-center p-0">
@@ -20,7 +20,7 @@ export default component$(() => {
                     </ul>
                 </div>
 
-                <div class="md:max-w-[320px] flex flex-col justify-start items-center gap-2  basis-[23%] bg-white text-[#003E88] rounded-xl pt-5">
+                <div class="md:max-w-[320px] flex flex-col justify-evenly items-center gap-2  basis-[23%] bg-white text-[#003E88] rounded-xl pt-5">
                     <img src={lances} alt="Lances facilitados" loading="lazy" width="80px" height="80px" />
                     <h2 class="text-xl min-h-[1.5em] text-left font-bold mx-2">LANCES FACILITADOS</h2>
                     <ul class="max-w-[90%] text-center flex flex-col items-center p-0">
@@ -31,7 +31,7 @@ export default component$(() => {
                     </ul>
                 </div>
 
-                <div class="md:max-w-[320px] flex flex-col justify-start items-center gap-2  basis-[23%] bg-white text-[#003E88] rounded-xl pt-5">
+                <div class="md:max-w-[320px] flex flex-col justify-evenly items-center gap-2  basis-[23%] bg-white text-[#003E88] rounded-xl pt-5">
                     <img src={planos} alt="Planos diferenciados" loading="lazy" width="80px" height="80px" />
                     <h2 class="text-xl min-h-[1.5em] text-left font-bold mx-2">PLANOS EXCLUSIVOS</h2>
                     <ul class="max-w-[90%] text-center flex flex-col items-center p-0">
@@ -42,7 +42,7 @@ export default component$(() => {
                     </ul>
                 </div>
 
-                <div class="md:max-w-[320px] flex flex-col justify-start items-center gap-2  basis-[23%] bg-white text-[#003E88] rounded-xl pt-5">
+                <div class="md:max-w-[320px] flex flex-col justify-evenly items-center gap-2  basis-[23%] bg-white text-[#003E88] rounded-xl pt-5">
                     <img class="max-w-[100px]" src={seg} alt="Transparência, segurança e credibilidade" loading="lazy" width="80px" height="80px" />
                     <h2 class="text-xl xl:min-h-[1.5em] text-center font-bold mx-1">TRANSPARÊNCIA, SEGURANÇA E CREDIBILIDADE</h2>
                     <ul class="max-w-[90%] text-center flex flex-col items-center p-0">
